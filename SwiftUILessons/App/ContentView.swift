9a76c3828d0afe34a90d650e327cd5c0dcb3c874
MaximB0nd <sersim11@yandex.ru>
2025-03-19 12:03:42 +0300

    Some UI elements

--- SwiftUILessons/App/ContentView.swift
+++ SwiftUILessons/App/ContentView.swift
@@ -8,15 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    var fff: String?
     var body: some View {
         var n: Int = 0
-        VStack {
+        
+        List {
+            Rectangle()
+                .frame(width: 20, height: 20)
+                .foregroundStyle(.red)
+            Circle()
+                .frame(width: 10, height: 10)
+                .foregroundStyle(.blue)
+            Divider()
+                .background(.black)
+            ProgressView()
+            ProgressView(value: 19, total: 80)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("\(n)")
             Button("Pick down", action: {
                 n+=1
+          
                 })
         }
         .padding()
