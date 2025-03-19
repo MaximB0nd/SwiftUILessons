f701e50710998ce78b90f2abeba984b693a2fab0
MaximB0nd <sersim11@yandex.ru>
2025-03-19 13:16:52 +0300

    Used some mod to UI

--- SwiftUILessons/App/ContentView.swift
+++ SwiftUILessons/App/ContentView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var fff: String?
+    @State var side = 100.0
+    
     var body: some View {
         var n: Int = 0
         
-        List {
+        ScrollView {
             Rectangle()
                 .frame(width: 20, height: 20)
                 .foregroundStyle(.red)
@@ -23,16 +24,48 @@ struct ContentView: View {
                 .background(.black)
             ProgressView()
             ProgressView(value: 19, total: 80)
+                .padding(40)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("\(n)")
+                .foregroundStyle(.red)
             Button("Pick down", action: {
                 n+=1
-          
-                })
+            })
+            .foregroundStyle(.primary)
+            .background(.green)
+            .clipShape(.capsule)
+            .frame(width: 100, height: 100)
+            .background(.yellow)
+            .clipShape(.buttonBorder)
+            
+            Image(systemName: "tree.fill")
+                .background(.yellow)
+                .padding(20)
+                .border(.red)
+                .frame(width: 120, height: 120)
+                .border(.red, width: 10)
+            
+            Rectangle()
+                .fill(Color.yellow)
+                .frame(minWidth: 10, idealWidth: 100, maxWidth: 300, minHeight: 10, idealHeight: 100, maxHeight: 300, alignment: .trailing)
+            
         }
         .padding()
+//        Rectangle()
+//            .frame(
+//                maxWidth: 25,
+//                maxHeight: 25)
+//            .border(.green, width: 3)
+//            .frame(maxWidth: side,
+//                   maxHeight: side)
+//            .border(.green, width: 3)
+//            .frame(maxWidth: .infinity,
+//                   maxHeight: .infinity)
+//        
+//        Slider(value: $side, in: 0...100, label: {Text("Side: \(side)")})
+//        Text("\(side)")
     }
 }
 
