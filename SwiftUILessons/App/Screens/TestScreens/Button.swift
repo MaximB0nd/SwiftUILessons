34547858c1df39a496c36a600cfe5462e8164e13
MaximB0nd <sersim11@yandex.ru>
2025-04-25 21:07:40 +0300

    Buttons p.1

--- SwiftUILessons/App/Screens/TestScreens/Button.swift
+++ SwiftUILessons/App/Screens/TestScreens/Button.swift
@@ -9,10 +9,52 @@ import SwiftUI
 
 struct ButtonsView: View {
     var body: some View {
-        Button(
-            "Tap me",
-            action: {print("tapeed")}
-        )
+        VStack (spacing: 30) {
+            Button(
+                "Tap me",
+                action: {print("tapeed")}
+            )
+            
+            Button(
+                "Tap me",
+                role: .destructive,
+                action: {print("tapeed")}
+            )
+            
+            Button {
+                    print("Меня нажали")
+            } label: {
+                HStack {
+                    Rectangle()
+                        .fill(.red)
+                        .frame(width: 50, height: 50)
+                    Text("Нажми меня")
+                    Image(systemName: "person.crop.square.fill")
+                }
+            }
+            
+            Button(role: .cancel)
+            {
+                    print("Меня нажали")
+                } label: {
+                    HStack {
+                        Rectangle().frame(width: 50, height: 50)
+                        Text("Нажми меня")
+                        Image(systemName: "person.crop.square.fill")
+                    }
+                }
+            Button {}
+                label: {
+                HStack {
+                        
+                    Text("TAP ME")
+                    .font(.largeTitle)
+                    Image(systemName: "lasso.badge.sparkles")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fill)
+                }.frame(width: 200, height: 5)
+                }
+        }
     }
 }
 
