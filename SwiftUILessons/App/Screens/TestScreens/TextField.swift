1b7f28ba6316bda64a89c5f1be56229ce9c9d122
MaximB0nd <sersim11@yandex.ru>
2025-05-14 18:02:10 +0300

    TextField

---
+++ SwiftUILessons/App/Screens/TestScreens/TextField.swift
@@ -0,0 +1,28 @@
+//
+//  TextField.swift
+//  SwiftUILessons
+//
+//  Created by Максим Бондарев on 14.05.2025.
+//
+
+import SwiftUI
+
+struct TextFields: View {
+    @State private var text = ""
+    var body: some View {
+        VStack {
+            TextField("Enter your name", text: .constant(""))
+            TextField("Enter your email", text: .constant(""))
+            TextField("Цитата", text: $text, prompt: Text("Цитата"), axis: .vertical)
+                .onSubmit{print(text)}
+            TextField("Число", value: .constant(0), format: .number)
+            
+            
+        }
+    }
+}
+
+
+#Preview {
+    TextFields()
+}
