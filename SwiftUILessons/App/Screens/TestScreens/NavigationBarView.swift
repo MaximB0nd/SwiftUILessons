2870e81a9333718073dfd3997ffb02ab7a18f790
MaximB0nd <sersim11@yandex.ru>
2025-05-15 13:16:21 +0300

    NavigationBarView p.1

---
+++ SwiftUILessons/App/Screens/TestScreens/NavigationBarView.swift
@@ -0,0 +1,33 @@
+//
+//  NavigationBar.swift
+//  SwiftUILessons
+//
+//  Created by Максим Бондарев on 15.05.2025.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        NavigationStack {
+            NavigationLink("New window") {
+                Text("new window")
+                    .navigationTitle("NavigationBar")
+                    .navigationBarTitleDisplayMode(.large)
+            }
+            
+            NavigationLink("Some windiow"){
+                Text("Some window")
+                    .toolbar{
+                        ToolbarItem(placement: .bottomBar, content: {Text("Some text")})
+                        ToolbarItem(placement: .principal, content: {Text("Hello")})
+                    }
+            }
+        
+        }
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}
